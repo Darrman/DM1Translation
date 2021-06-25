26d7dd23a13f86dac00dd822d4984b3911804f91
Darrman <30606600+Darrman@users.noreply.github.com>
2021-06-25 03:42:17 +0100

    Worked on the remaining menus
    Entering the deck inexplicably crashed the game with an invalid opcode, but the data was exactly the same. Figuring out what's causing this is important.

--- Insertion/Insert.bat
+++ Insertion/Insert.bat
@@ -7,6 +7,7 @@ perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/cardname.txt"
 perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/dueltext.txt"
 perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/dialogue.txt"
 perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/bank1.txt"
+::perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/tilemaps.txt"
 
 
 
