6ec43bec96737b8fb97b94f68376eb0596c8e9c4
Darrman <30606600+Darrman@users.noreply.github.com>
2021-09-21 01:29:43 +0100

    Card descriptions, part 1
    First fifty done. That'll be enough for the night.

--- Insertion/Insert.bat
+++ Insertion/Insert.bat
@@ -4,6 +4,7 @@
 )
 
 perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/cardname.txt"
+perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/carddesc.txt"
 perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/dueltext.txt"
 perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/dialogue.txt"
 perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/bank1.txt"
