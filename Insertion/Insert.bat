66d1144c619841f8ea52d8659b8d27b7c303ed29
Darrman <30606600+Darrman@users.noreply.github.com>
2021-11-16 00:54:23 +0000

    Credits inserted
    They still need adjustment for fan translation credits. I've freed plenty of space for it. ...Too much space, honestly. I hope I don't have to pad with crediting half of 4kids for the dub names.
    After this, it's proofread for squished tiles, extend monster names where possible, and fix that menu bug.

--- Insertion/Insert.bat
+++ Insertion/Insert.bat
@@ -10,6 +10,7 @@ perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/dialogue.txt"
 perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/bank1.txt"
 perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/tilemaps.txt"
 perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/password.txt"
+perl ..\abcde.pl -cm abcde::Atlas "DM1.gb" "script/credits.txt"
 
 
 
